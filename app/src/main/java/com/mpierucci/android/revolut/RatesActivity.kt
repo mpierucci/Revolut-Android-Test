d8d3347f0428431acfb90edbaa9ddf339ab710c3
Marco <marco1pierucci@gmail.com>
2020-04-17 19:31:05 +0100

    Initial commit

---
+++ app/src/main/java/com/mpierucci/android/revolut/RatesActivity.kt
@@ -0,0 +1,12 @@
+package com.mpierucci.android.revolut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RatesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rates)
+    }
+}
